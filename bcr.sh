d86cf8ee7fb51d29352d02ceb21095e940f582cc
Mathis <120995618+shell-don@users.noreply.github.com>
2024-06-09 20:36:56 +0200

    Déchiffrement

---
+++ bcr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#!/bin/zsh
+
+echo "Quel est le chemin du fichier ?" 
+read path
+echo "Combien de fois voulez-vous le déchiffrer ?"
+read n 
+
+i=1
+until [ $i -gt $n ]
+  do 
+    aescrypt -d $path 
+    [[ $i -le $n ]] && rm $path || echo "L'Output $i n'est pas supprimé"
+    path=$(sed 's/.aes//' <<< $path)
+    echo "Déchiffrement $i effectué"  
+    ((i++))
+  done
