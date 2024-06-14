ab93f23494b9225d9f590286b58b672d4d14ad35
Mathis <120995618+shell-don@users.noreply.github.com>
2024-06-14 20:03:59 +0200

    Chiffrement (version Alpha)

---
+++ Alpha/bc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Quel est le chemin du fichier ?" 
+read path
+echo "Combien de fois voulez-vous le chiffrer ?"
+read n 
+
+i=1
+until [ $i -gt $n ]
+  do 
+    aescrypt -e $path 
+    [[ $i -le $n ]] && rm $path || echo "L'Output $i n'est pas supprimé"
+    path=${path}.aes
+    echo "Chiffrement $i effectué"  
+    ((i++))
+  done
+
